7b3190164a11cad1ad13d344f89803a09ed0de57
David Mallard <correspondence@davidmallard.id.au>
2018-02-22 14:26:56 +1100

    Convert the get_senators response into a tibble

--- R/get-senators.R
+++ R/get-senators.R
@@ -18,5 +18,26 @@ get_senators <- function(date = NULL, party = NULL, state = NULL,
 getSenators <- function(date = NULL, party = NULL, state = NULL,
                         search = NULL) {
   params <- params_from_call(match.call())
-  do.call("call_api", params)
+  do.call("call_api", params) %>%     # Call the generic API function
+    tibble::as_tibble() %>%           # Convert the response to a tibble
+    readr::type_convert(              # By default all variables are strings
+      col_types = readr::cols(        # Specify all column types
+        member_id = readr::col_integer(),
+        house = readr::col_integer(),
+        first_name = readr::col_character(),
+        last_name = readr::col_character(),
+        constituency = readr::col_character(),
+        party = readr::col_character(),
+        entered_house = readr::col_date(),
+        left_house = readr::col_date(),
+        entered_reason = readr::col_character(),
+        left_reason = readr::col_character(),
+        person_id = readr::col_integer(),
+        title = readr::col_character(),
+        lastupdate = readr::col_datetime(),
+        full_name = readr::col_character(),
+        name = readr::col_character(),
+        image = readr::col_character()
+      )
+    )
 }
