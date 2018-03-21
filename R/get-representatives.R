3c9c735651aa3a6e8a365af78f57a695642af881
David Mallard <correspondence@davidmallard.id.au>
2018-03-21 17:22:40 +1100

    Process the get_representatives data to output a consistent tidy format

--- R/get-representatives.R
+++ R/get-representatives.R
@@ -18,26 +18,78 @@ get_representatives <- function(postcode = NULL, date = NULL, party = NULL,
 getRepresentatives <- function(postcode = NULL, date = NULL, party = NULL,
                                search = NULL) {
   params <- params_from_call(match.call())
-  do.call("call_api", params) %>%     # Call the generic API function
-    tibble::as_tibble() %>%           # Convert the response to a tibble
-    readr::type_convert(              # By default all variables are strings
-      col_types = readr::cols(        # Specify all column types
-        member_id = readr::col_integer(),
-        house = readr::col_integer(),
-        first_name = readr::col_character(),
-        last_name = readr::col_character(),
-        constituency = readr::col_character(),
-        party = readr::col_character(),
-        entered_house = readr::col_date(),
-        left_house = readr::col_date(),
-        entered_reason = readr::col_character(),
-        left_reason = readr::col_character(),
-        person_id = readr::col_integer(),
-        title = readr::col_character(),
-        lastupdate = readr::col_datetime(),
-        full_name = readr::col_character(),
-        name = readr::col_character(),
-        image = readr::col_character()
+  robj <- do.call("call_api", params)
+
+  # Separate the list into those with office data and those without
+  has_office <- purrr::map_lgl(robj, ~ purrr::has_element(names(.x), "office"))
+  reps_office <- robj[has_office]
+  reps_no_office <- robj[!has_office]
+
+  # Convert each sub-list into a tibble
+  reps_office_tbl <- tibble::tibble(
+    member_id = purrr::map_chr(reps_office, "member_id"),
+    house = purrr::map_chr(reps_office, "house"),
+    first_name = purrr::map_chr(reps_office, "first_name"),
+    last_name = purrr::map_chr(reps_office, "last_name"),
+    constituency = purrr::map_chr(reps_office, "constituency"),
+    party = purrr::map_chr(reps_office, "party"),
+    entered_house = purrr::map_chr(reps_office, "entered_house"),
+    left_house = purrr::map_chr(reps_office, "left_house"),
+    entered_reason = purrr::map_chr(reps_office, "entered_reason"),
+    left_reason = purrr::map_chr(reps_office, "left_reason"),
+    person_id = purrr::map_chr(reps_office, "person_id"),
+    title = purrr::map_chr(reps_office, "title"),
+    lastupdate = purrr::map_chr(reps_office, "lastupdate"),
+    full_name = purrr::map_chr(reps_office, "full_name"),
+    name = purrr::map_chr(reps_office, "name"),
+    image = purrr::map_chr(reps_office, "image"),
+    office = purrr::map(
+      reps_office,
+      ~ tibble::tibble(
+        moffice_id = purrr::map_chr(.x[["office"]], "moffice_id"),
+        dept = purrr::map_chr(.x[["office"]], "dept"),
+        position = purrr::map_chr(.x[["office"]], "position"),
+        from_date = purrr::map_chr(.x[["office"]], "from_date"),
+        to_date = purrr::map_chr(.x[["office"]], "to_date"),
+        person = purrr::map_chr(.x[["office"]], "person"),
+        source = purrr::map_chr(.x[["office"]], "source")
       )
     )
+  )
+
+  reps_no_office_tbl <- tibble::tibble(
+    member_id = purrr::map_chr(reps_no_office, "member_id"),
+    house = purrr::map_chr(reps_no_office, "house"),
+    first_name = purrr::map_chr(reps_no_office, "first_name"),
+    last_name = purrr::map_chr(reps_no_office, "last_name"),
+    constituency = purrr::map_chr(reps_no_office, "constituency"),
+    party = purrr::map_chr(reps_no_office, "party"),
+    entered_house = purrr::map_chr(reps_no_office, "entered_house"),
+    left_house = purrr::map_chr(reps_no_office, "left_house"),
+    entered_reason = purrr::map_chr(reps_no_office, "entered_reason"),
+    left_reason = purrr::map_chr(reps_no_office, "left_reason"),
+    person_id = purrr::map_chr(reps_no_office, "person_id"),
+    title = purrr::map_chr(reps_no_office, "title"),
+    lastupdate = purrr::map_chr(reps_no_office, "lastupdate"),
+    full_name = purrr::map_chr(reps_no_office, "full_name"),
+    name = purrr::map_chr(reps_no_office, "name"),
+    image = purrr::map_chr(reps_no_office, "image"),
+    office = purrr::map(
+      reps_no_office,
+      ~ tibble::tibble(
+        moffice_id = character(),
+        dept = character(),
+        position = character(),
+        from_date = character(),
+        to_date = character(),
+        person = character(),
+        source = character()
+      )
+    )
+  )
+
+  # Join the sub-tibbles back into a single dataset
+  reps_tbl <- dplyr::bind_rows(reps_office_tbl, reps_no_office_tbl)
+
+  reps_tbl
 }
