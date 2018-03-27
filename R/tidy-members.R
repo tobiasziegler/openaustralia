26a664935e40cccca022abd95c29eeadc32fca7e
David Mallard <correspondence@davidmallard.id.au>
2018-03-27 17:39:50 +1100

    Fix the conversion of office data into nested tibbles and improve the member list functions

--- R/tidy-members.R
+++ R/tidy-members.R
@@ -2,32 +2,20 @@
 #'
 #' @param data A nested list (list of lists) with each item in the top-level
 #'   list representing a member
-#' @return A tibble with each member represented by one row
+#' @return A tibble with each member represented by one row.
 tidy_members <- function(data) {
-  # Separate the list into those with office data and those without
-  has_office <- purrr::map_lgl(data, ~ purrr::has_element(names(.x), "office"))
-  members_office <- data[has_office]
-  members_no_office <- data[!has_office]
-
-  # Convert each sub-list into a tibble
-  members_office_tbl <- tibble_members(members_office, TRUE)
-  members_no_office_tbl <- tibble_members(members_no_office, FALSE)
-
-  # Join the sub-tibbles back into a single dataset
-  members_tbl <- dplyr::bind_rows(members_office_tbl, members_no_office_tbl)
+  # Convert the list into a tibble
+  members_tbl <- tibble_members(data)
 
   members_tbl
 }
 
-#' Convert a sub-list of members into a nested tibble
+#' Convert a list of members into a nested tibble
 #'
 #' @param data A nested list (list of lists) with each item in the top-level
-#'   list representing a member. All members must have office data or must not
-#'   have office data
-#' @param has_office A boolean value indicating whether the sub-list contains
-#'   office data
-#' @return A tibble with each member represented by one row
-tibble_members <- function(data, has_office) {
+#'   list representing a member.
+#' @return A tibble with each member represented by one row.
+tibble_members <- function(data) {
   tibble::tibble(
     member_id = purrr::map_chr(data, "member_id", .null = NA_character_),
     house = purrr::map_chr(data, "house", .null = NA_character_),
@@ -45,32 +33,38 @@ tibble_members <- function(data, has_office) {
     full_name = purrr::map_chr(data, "full_name", .null = NA_character_),
     name = purrr::map_chr(data, "name", .null = NA_character_),
     image = purrr::map_chr(data, "image", .null = NA_character_),
-    office = ifelse(
-      has_office,
-      purrr::map(
-        data,
-        ~ tibble::tibble(
-          moffice_id = purrr::map_chr(.x[["office"]], "moffice_id", .null = NA_character_),
-          dept = purrr::map_chr(.x[["office"]], "dept", .null = NA_character_),
-          position = purrr::map_chr(.x[["office"]], "position", .null = NA_character_),
-          from_date = purrr::map_chr(.x[["office"]], "from_date", .null = NA_character_),
-          to_date = purrr::map_chr(.x[["office"]], "to_date", .null = NA_character_),
-          person = purrr::map_chr(.x[["office"]], "person", .null = NA_character_),
-          source = purrr::map_chr(.x[["office"]], "source", .null = NA_character_)
-        )
-      ),
-      purrr::map(
-        data,
-        ~ tibble::tibble(
-          moffice_id = character(),
-          dept = character(),
-          position = character(),
-          from_date = character(),
-          to_date = character(),
-          person = character(),
-          source = character()
-        )
-      )
-    )
+    office = purrr::map(data, tibble_office)
   )
+}
+
+#' Convert a list of offices held by a member into a tibble, or create an
+#' empty tibble if the member has no office data.
+#'
+#' @param data A list with each item representing an office held by the member.
+#'
+#' @return A tibble with each office represented by one row.
+tibble_office <- function(data) {
+  if ("office" %in% names(data)) {
+    office <- tibble::tibble(
+      moffice_id = purrr::map_chr(data[["office"]], "moffice_id", .null = NA_character_),
+      dept = purrr::map_chr(data[["office"]], "dept", .null = NA_character_),
+      position = purrr::map_chr(data[["office"]], "position", .null = NA_character_),
+      from_date = purrr::map_chr(data[["office"]], "from_date", .null = NA_character_),
+      to_date = purrr::map_chr(data[["office"]], "to_date", .null = NA_character_),
+      person = purrr::map_chr(data[["office"]], "person", .null = NA_character_),
+      source = purrr::map_chr(data[["office"]], "source", .null = NA_character_)
+    )
+  } else {
+    office <- tibble::tibble(
+      moffice_id = character(),
+      dept = character(),
+      position = character(),
+      from_date = character(),
+      to_date = character(),
+      person = character(),
+      source = character()
+    )
+  }
+
+  office
 }
